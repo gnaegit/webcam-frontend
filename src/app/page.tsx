18b486c49bd2ec39710687992cba43bf7554913f
gnaegit <tobi.gnaegi@gmail.com>
2025-06-19 10:24:22 +0200

    add Button to go to stats page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,18 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4">
       <h1 className="text-3xl font-bold mb-6">Welcome</h1>
-      <Link href="/camera">
-        <Button variant="default" size="lg">
-          Go to Camera Page
-        </Button>
-      </Link>
+      <div className="flex flex-col gap-4">
+        <Link href="/camera">
+          <Button variant="default" size="lg">
+            Go to Camera Page
+          </Button>
+        </Link>
+        <Link href="/stats">
+          <Button variant="default" size="lg">
+            Go to Stats Page
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
